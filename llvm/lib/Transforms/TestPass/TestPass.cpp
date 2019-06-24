2fe57ca968db59a68fad4d0ddf65baf05d3dca74
Carson Harmon <carson.harmon@trailofbits.com>
2019-06-24 09:33:35 -0400

    Some minor tweaks

--- llvm/lib/Transforms/TestPass/TestPass.cpp
+++ llvm/lib/Transforms/TestPass/TestPass.cpp
@@ -1,5 +1,6 @@
+
 //Types/Headers
-#include "llvm/IR/Module.h"
+#include "llvm/Transforms/Instrumentation/TestSanitizer.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/InstrTypes.h"
@@ -45,30 +46,11 @@ namespace {
 
 bool TestSanitizerModule::runOnModule(Module &module) {
 	errs() << "This is the LLVM Module for source file:  " << module.getSourceFileName() << "\n"; 	
-
-	//This is the LLVM context, this contains important data and is used to make most important things 
-	LLVMContext& context = module.getContext();
-	IRBuilder<> builder(context);
-
-	//The LLVM module class has a list of all functions in the module, I just picked the first one in the list. 
-	Function& single_function = *(module.begin()); 
-	builder.SetInsertPoint(&(single_function.getEntryBlock().front()));
-
-	//Create a string
-	Value *hello_world_str = builder.CreateGlobalStringPtr("Hello World!\n");
-
-	//Create the prototype for puts function 
-	std::vector<llvm::Type *> puts_args;
-	puts_args.push_back(builder.getInt8Ty()->getPointerTo());
-	ArrayRef<Type*> puts_args_ref(puts_args);
-
-	//Create function and insert call into IR
-	FunctionType *puts_type =
-		FunctionType::get(builder.getInt32Ty(), puts_args_ref, false);
-	Constant *putsFunc = module.getOrInsertFunction("puts", puts_type);
-	builder.CreateCall(putsFunc, hello_world_str);
-
-	return true;
+	for (auto &func : module) {
+		errs() << "Hello function: " << func.getName() << "!\n"; 
+	}
+	//Makes no changes
+	return false;
 }
 
 
@@ -92,7 +74,7 @@ bool TestSanitizerFunction::runOnFunction(Function &func) {
 		FunctionType::get(llvm::Type::getVoidTy(context), name_set_args_ref, false);
 
 	//Create the function and use the IR Builder to create a function call
-	Constant * name_set_func = func.getParent()->getOrInsertFunction("LeakCheck_SetFunctionName", name_set_func_type);
+	Constant * name_set_func = func.getParent()->getOrInsertFunction("testsan_HelloFunction", name_set_func_type);
 	Value * name_val = builder.CreateGlobalStringPtr(func.getName());
  	Value * args[] = {name_val};
 	builder.CreateCall(name_set_func, args);
@@ -110,7 +92,7 @@ bool TestSanitizerFunction::runOnFunction(Function &func) {
 					FunctionType * summary_func_type = 
 							FunctionType::get(llvm::Type::getVoidTy(context), summary_args_ref, false);
 
-					Constant * summary_func = func.getParent()->getOrInsertFunction("LeakCheck_PrintLeakSummary", summary_func_type);
+					Constant * summary_func = func.getParent()->getOrInsertFunction("testsan_EndOfMain", summary_func_type);
 					builder.CreateCall(summary_func, {});
 				}
 			}
@@ -121,22 +103,19 @@ bool TestSanitizerFunction::runOnFunction(Function &func) {
 
 //check for object allocation instruction and grab the pointer after it, print it. 
 bool TestSanitizerBlock::runOnBasicBlock(BasicBlock &BB) {
-	
+		
 	LLVMContext& context = BB.getContext();
 	Module* current_module = BB.getModule(); 	
-	
+
+	//This is similar to the previous pass 	
 	PointerType * byte_ptr_type = llvm::Type::getInt8Ty(context)->getPointerTo(); 	
-
-	errs() << "got void ty\n"; 
-
 	std::vector<llvm::Type *> log_allocation_args;
 	log_allocation_args.push_back(byte_ptr_type);
-	errs() << "got ptr 2\n"; 
 	ArrayRef<Type*> log_allocation_ref(log_allocation_args);
 
 	FunctionType *log_allocation_type = FunctionType::get(byte_ptr_type, log_allocation_ref, false);
 	
-	Constant *log_allocation_func = current_module->getOrInsertFunction("LeakCheck_LogAllocation", log_allocation_type);
+	Constant *log_allocation_func = current_module->getOrInsertFunction("testsan_AfterMalloc", log_allocation_type);
 	
 	errs() << "Checking instructions\n"; 	
 	for (auto& Inst : BB) {
@@ -157,6 +136,7 @@ bool TestSanitizerBlock::runOnBasicBlock(BasicBlock &BB) {
 			}
 		}
 	}
+	
 	return true; 
 }
 
@@ -166,6 +146,19 @@ char TestSanitizerFunction::ID = 0;
 char TestSanitizerBlock::ID = 0;  
 
 //For registering with opt for debugging/testing
+
 static RegisterPass<TestSanitizerModule> TestMod("testmod", "testing module pass"); 
 static RegisterPass<TestSanitizerFunction> TestFunc("testfunc", "testing function pass");  
 static RegisterPass<TestSanitizerBlock> TestBlock("testblock", "testing block pass");  
+ 
+/*
+ModulePass * llvm::createSimpleModulePass() {
+	return new TestSanitizerModule(); 
+}
+FunctionPass * llvm::createSimpleFuncPass() {
+	return new TestSanitizerFunction(); 
+}
+BasicBlockPass * llvm::createSimpleBlockPass() {
+	return new TestSanitizerBlock(); 
+}
+*/ 
--- target_programs/malloc_target.c
+++ target_programs/malloc_target.c
@@ -1,17 +1,12 @@
 #include <stdlib.h> 
-#include <string.h> 
-#include <stdio.h> 
 
-char * foo(int size) {
-		return malloc(sizeof(char) * size); 
+//Just allocate a buffer of size 10
+char * foo() {
+		return malloc(sizeof(char) * 10); 
 }
 
 int main(int argc, char * argv[]) {
 
-	if (argc < 2) {
-		printf("Provide a size for malloc!\n Ex: ./malloc_target 10\n"); 
-		return -1; 	
-	}
-	char * some_ptr = foo(atoi(argv[1]));
+	char * some_ptr = foo();
 	return 0; 
 }
